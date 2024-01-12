f7a06843682e41c557287ee7c5461b23e0c5f8a3
adenkesuma <adenfdfd10@gmail.com>
2024-01-12 21:08:07 +0700

    add setup mongodb connection

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,48 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// mongodb config
+const uri = `mongodb+srv://${process.env.USERNAME_MONGODB}:${process.env.PASSWORD_MONGODB}@cluster0.ntdbhbi.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    // db and collections
+    const menuCollections = client.db("foodi").collection("menu");
+    const cartCollections = client.db("foodi").collection("cart");
+
+    // all menu items operations
+    app.get("/menu", async(req, res) => {
+      const result = await menuCollections.find().toArray();
+      res.send(result);
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send('hello bro')
