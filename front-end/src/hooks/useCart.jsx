fe9d63ef82b99becfd04b7ffd1ca9af1f2448d84
adenkesuma <adenfdfd10@gmail.com>
2024-01-14 11:05:25 +0700

    add useCart context

---
+++ front-end/src/hooks/useCart.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthProvider";
+import { useQuery } from "@tanstack/react-query";
+
+const useCart = () => {
+  const { user } = useContext(AuthContext);
+
+  const { refetch ,data: cart = []} = useQuery({
+    queryKey: ["carts", user?.email],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:3000/carts?email=${user?.email}`)
+      return res.json();
+    }
+  })
+
+  return [cart, refetch]
+}
+
+export default useCart;
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -4,9 +4,18 @@ import { RouterProvider } from 'react-router-dom'
 import router from './router/Router.jsx'
 import AuthProvider from './contexts/AuthProvider.jsx'
 
+import {
+  QueryClient,
+  QueryClientProvier
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
-    <RouterProvider router={router} />
+    <QueryClientProvier client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvier>
   </AuthProvider>
 )
 
