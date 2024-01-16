c39d6db204e6c262d3a1d87f03f3adecc3fea62c
adenkesuma <adenfdfd10@gmail.com>
2024-01-16 15:55:02 +0700

    `feat:` finish dashboard layout style

--- front-end/src/layout/DashboardLayout.jsx
+++ front-end/src/layout/DashboardLayout.jsx
@@ -1,20 +1,52 @@
 import { Link, Outlet } from "react-router-dom";
-import { ShoppingBag, Menu, PlusCircle, FilePenLine, LayoutDashboard, Users } from "lucide-react";
+import { ShoppingBag, Menu, PlusCircle, FilePenLine, LayoutDashboard, Users, Home, ShoppingCart, Send, ShieldQuestion } from "lucide-react";
+
+const sharedLinks = (
+  <>
+    <li>
+      <Link to="/dashboard">
+        <Home className="w-4" />
+        <span>Home</span>
+      </Link>
+    </li>
+    <li>
+      <Link to="/dashboard">
+        <ShoppingCart className="w-4" />
+        <span>Menu</span>
+      </Link>
+    </li>
+    <li>
+      <Link to="/dashboard">
+        <Send className="w-4" />
+        <span>Order Tracking</span>
+      </Link>
+    </li>
+    <li>
+      <Link to="/dashboard">
+        <ShieldQuestion className="w-4" />
+        <span>Customer Support</span>
+      </Link>
+    </li>
+  </>
+)
 
 const DashboardLayout = () => {
   return (
     <div>
       <div className="drawer lg:drawer-open">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col items-center justify-center">
-          <div className="w-full px-8 py-2 flex items-center justify-between">
+        <div className="drawer-content flex flex-col sm:items-start items-center sm:justify-start">
+          <div className="w-full px-4 py-2 flex items-center justify-between">
             <label htmlFor="my-drawer-2" className="py-2 px-4 bg-primary rounded-xl drawer-button lg:hidden">
               <Menu className="w-4 text-white" />
             </label>
 
-            <button className="px-6 py-2 rounded-xl bg-primary text-white font-semibold text-sm">Logout</button>
+            <button className="px-6 py-2 rounded-xl bg-primary text-white font-semibold text-sm lg:hidden">Logout</button>
           </div>
-          <Outlet />
+
+          <div className="mt-5 md:mt-2 mx-4">
+            <Outlet />
+          </div>
         </div> 
         <div className="drawer-side">
           <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> 
@@ -58,6 +90,10 @@ const DashboardLayout = () => {
                 <span>Users</span>
               </Link>
             </li>
+
+            <div className="w-[80%] ml-4 border-t border-gray-500 my-8" />
+
+            {sharedLinks}
           </ul>
 
         </div>
