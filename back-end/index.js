3c547ab6abf8f203555b8b90a51728ec5b80fb5b
adenkesuma <adenfdfd10@gmail.com>
2024-01-16 23:16:44 +0700

    add post jwt authentication

--- back-end/index.js
+++ back-end/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 import MenuRoutes from "./routes/MenuRoutes.js";
 import CartRoutes from "./routes/CartRoutes.js";
 import UserRoutes from "./routes/UserRoutes.js";
@@ -9,10 +10,6 @@ import UserRoutes from "./routes/UserRoutes.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// middleware
-app.use(cors());
-app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.USERNAME_MONGODB}:${process.env.PASSWORD_MONGODB}@cluster0.ntdbhbi.mongodb.net/foodi`;
 
@@ -30,13 +27,44 @@ db.once("open", () => {
 
 mongoose.Promise = global.Promise;
 
+// jwt authentication
+app.post("/jwt", async(req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_JWT, {
+    expiresIn: "1h"
+  });
+
+  res.json({ token });
+});
+
+// verify jwt token
+const verifyToken = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send("Unauthorized access");
+  }
+
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESSA_TOKEN_JWT, (err, decoded) => {
+    if (err) {
+      res.status(401).send("Token in invalid!");
+    }
+
+    req.decoded = decoded;
+    next();
+  })
+}
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("welcome to foodi server");
+});
+
 app.use("/menu", MenuRoutes);
 app.use("/carts", CartRoutes);
 app.use("/users", UserRoutes);
 
-app.get("/", (req, res) => {
-  res.send("hello bro");
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port http://localhost:${PORT}`);
